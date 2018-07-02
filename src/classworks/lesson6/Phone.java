bf1913937c1d16e46ec8d70b2ae43662c4c1a029
GreatUrsa <thegreat.bear@mail.ru>
2018-07-02 21:28:33 +0300

    add 2 files

---
+++ src/classworks/lesson6/Phone.java
@@ -0,0 +1,7 @@
+package classworks.lesson6;
+
+public class Phone {
+    String number;
+    String model;
+    double weight;
+}
---
+++ src/classworks/lesson6/PhoneDemo.java
@@ -0,0 +1,17 @@
+package classworks.lesson6;
+
+public class PhoneDemo {
+    public void setMyPhone(Phone myPhone) {
+
+
+
+    Phone myPhone1 = new Phone();
+    Phone myPhone2 = new Phone();
+    Phone myPhone3 = new Phone();
+
+    myPhone1.number= "0678269585";
+    myPhone1.model = "LG";
+    myPhone1.weight = 154.3;
+
+    }
+}
